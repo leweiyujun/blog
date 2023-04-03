25f04971ed74ff964e6c5dec0a6c655365e2131e
leweijun <592958446@qq.com>
2023-04-03 15:16:17 +0800

    20230403

---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,16 @@
+module.exports = {
+    '/resources/website': require('../resources/website/sidebar'),
+    '/notes/vuepress': require('../notes/vuepress/sidebar'),
+    '/notes/vue': require('../notes/vue/sidebar'),
+    '/notes/react': require('../notes/react/sidebar'),
+    '/notes/webpack': require('../notes/webpack/sidebar'),
+    '/notes/ide': require('../notes/ide/sidebar'),
+    '/notes/git': require('../notes/git/sidebar'),
+    '/notes/h5c3': require('../notes/h5c3/sidebar'),
+    '/notes/js': require('../notes/js/sidebar'),
+    '/notes/less': require('../notes/less/sidebar'),
+    '/notes/nodejs': require('../notes/nodejs/sidebar'),
+    // '/otherNotes/python': require('../otherNotes/python/sidebar'),
+    // '/projects/covid19': require('../projects/covid19/sidebar'),
+    // '/projects/pink-echarts': require('../projects/pink-echarts/sidebar'),
+  }
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /img/logo.png
+heroText: 乐维的学习文档
+tagline: 某位老师曾经说过，你不需要多大的能力，但你需要有解决问题的能力
+actionText: 开始探索
+actionLink: /notes/h5c3/css
+features:
+- title: uniapp
+  details: 擅长uniapp开发各类小程序，其中微信小程序各项api尤为熟悉。
+- title: nodejs
+  details: 擅长利用nodejs开发服务端后台，熟悉微信支付等接口的调用。
+- title: vue-element-admin
+  details: 擅长利用vue-element-admin模板搭建管理后台。
+footer: 本站部分内容来自互联网，如若侵权，联系leweicode@foxmail.com | 部分原创内容归本站所有人乐维所有
+---
